8c240b2c9ce50c02d313758fae8a280517187ebc
UsingPP <wsx1386@gmail.com>
2024-04-19 21:12:36 +0900

    절반 완성

--- frontend/src/pages/Surveys/LeadershipSurvey/Result.jsx
+++ frontend/src/pages/Surveys/LeadershipSurvey/Result.jsx
@@ -8,6 +8,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 import Button from 'react-bootstrap/Button';
 import { positions } from '@mui/system';
 import 'bootstrap/dist/css/bootstrap.css';
+import { red } from '@mui/material/colors'
 const init_data = {
   "leadership_score_self": "",
   "leadership_mean_by_sector": {
@@ -23,6 +24,7 @@ const init_data = {
 }
 
 function Result(props) {
+  const temp = [1,2,3,4,5]
   const lang = props.language
   const classes = useStyles();
   const data = dt[lang].data
@@ -30,11 +32,12 @@ function Result(props) {
   console.log(data)
   const discretes = data;
   const progressColor = ["success","warning","danger","danger" ]
-  const backColor1 = ["#7FFF00C2","#FFFF00C2",	"#FF4500C2"	 ]
-  const backColor2 = ["#7FFF0030","#FFFF0030",	"#FF450030"	 ]
-  const borderColor = ["#32CD32C2","#FFD700C2",	"	#DC143CC2"	 ]
   const [resultdata, setresultdata] = useState(1);
   const [isLoading, setIsLoading] = useState(true)
+
+  const [isHovering, setisHorving] = useState(false);
+  const [hoveingNum, setHoveingNum] = useState(1);
+
   function appraise_level(score){
     if (score<35){
       return 2;
@@ -43,7 +46,7 @@ function Result(props) {
     }else{
       return 0;
     }
-  }
+  };
 
   const fetchData = async () => {
     try {
@@ -57,7 +60,7 @@ function Result(props) {
         body: JSON.stringify({ survey_name: "LeadershipSurvey" })
       });
       const resultdata = await response.json();
-      console.log(resultdata)
+      //console.log(resultdata)
       setresultdata(resultdata);
       setIsLoading(false)
     } catch (error) {
@@ -67,7 +70,12 @@ function Result(props) {
       useEffect(() => {
         fetchData();
               }, []);
-
+  function handleMouseOver(num)
+  {
+    setisHorving(true);
+    setHoveingNum(num);
+  };
+  const handleMouseOut = () => { setisHorving(false); };
   return (
     <Container className={classes.background_box} maxWidth="lg" sx={{ borderRadius: '16px' }}>
     <div>
@@ -75,6 +83,8 @@ function Result(props) {
     </div>
     <br></br>
       <Grid container  >
+
+        {/* Title */}
         <Grid item xs={12}sx={{ position: 'relative' }} >
           <Box sx={{ marginX: "20%", marginTop: "20px", marginBottom: "12px"}}>
             <Typography variant='h2' align='center' sx = {{ borderBottom: "1px solid black", fontFamily : "'Source Serif 4'", fontSize : 72, fontWeight : 700}}>
@@ -85,96 +95,78 @@ function Result(props) {
               </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12}>
-          <Grid container>
-            <Grid item md={6} xs = {12}>
-              <RadarChartLeadershipData language = {lang}></RadarChartLeadershipData>
-            </Grid>
-            <Grid item md={6} xs = {12} paddingX = "10px !important">
-              <Box >
-                <Typography variant="h5" sm = {{marginY:"1%"}}md = {{marginLeft: "5%" ,marginY:"2%"}}>
-                  Total Point
-                </Typography>
-                <Typography variant="h1" sx = {{fontFamily : "'Source Serif 4'", fontWeight : "600"}} align="center" >
-                {isLoading ? <CircularProgress/> : <SlotCounter value={() => {
-                  // 배열의 평균 계산
-                  const values = Object.values(resultdata["leadership_mean_by_sector"]);
-                  const sum = values.reduce((acc, cur) => acc + cur, 0);
-                  const average = sum / values.length;
-                  return average.toFixed(1);
-                }} />}
 
-                </Typography>
+        {/* Body */}
+        <Box>
+          {temp.map((none, index) => {
+            return (
+              <Grid container>
+                <Grid item xs={4} back>
+                  <Typography fontWeight={600} variant="subtitle1" align='center'> 이미지 </Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography fontWeight={600} variant="subtitle1" align='center'> 내 용 이 다 ! ! ! </Typography>
+                </Grid>
+              </Grid>
+            );
+            })}
 
+            <br/>
+            <br/>
+            <br/>
+          {temp.map((none, index) => {
+            return (
+              <Grid container>
+                <Grid item xs= {12} sm = {1}>
+                  {index == 0 ? "진짜 타이틀" : ""}
+                </Grid>
+                <Grid container xs = {12} sm = {11}
+                onMouseOver = {() => handleMouseOver(index)}
+                onMouseOut = {handleMouseOut}>
 
-              </Box>
-              <br></br>
-              <Box >
-                <Typography variant="h5" marginLeft="5%" marginY="2%">
-                  <Box >Category</Box>
-                </Typography>
-                {isLoading ?<CircularProgress/> : discretes.map((discrete) =>{
-                  const int = resultdata["leadership_mean_by_sector"][discrete["code"]].toFixed(1)
-                  console.log(int, progressColor[Math.floor(int/20)])
-                  const index = appraise_level(int)
-                  return (
-                  <Paper sx = {{marginY : "12px" , paddingY : "5px"}}>
-                  <Grid container>
-                    <Grid item xs={12} sm = {4}>
-                      <Typography fontWeight={600} variant="subtitle1" align='center'>{discrete.name}</Typography>
+                    <Grid item xs = {12} sm = {3} >
+                      <img src="https://www.thebalancemoney.com/thmb/1UmFPhRa1-r2sJ66xktyX4MnQ_E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/leadership-definition-2948275-Final-edit-cc7103c8ac254917a692c1d5c378a169.jpg" width={"250px"} height={"200px"}/>
                     </Grid>
-                    <Grid item xs={12} sm = {8}>
-                      <ProgressBar  animated label = {int} variant={progressColor[index]} now={int} />
+
+                    <Grid item xs={12} sm = {6} sx = {{height : '200px'}}>
+                          <Box onMouseOver = {() => handleMouseOver(index)}>
+                          { isHovering && index === hoveingNum  ? ( 
+                            <Typography fontWeight={600} variant="subtitle1" align='center'>
+
+                               "남을 비판하고 싶을 땐 언제나 이 점을 명심하거라" <br/>
+                            아버지는 이렇게 말씀하셨다. <br/>
+                            위대한 개츠비, F. 스콧 피츠제럴드 
+                            </Typography>
+                          ) : "123" }
+                          </Box>
+
                     </Grid>
-                  </Grid>
-                  </Paper>
-                )}) }
-              </Box>
-            </Grid>
-          </Grid>
+                </Grid>
+              </Grid>
+            );
+            })}
+
+            <br/>
+            <br/>
+            <br/>
+          {temp.map((none, index) => {
+            return (
+              <Grid container xs = {12}>
+                <Grid item xs={4} back>
+                  <Typography fontWeight={600} variant="subtitle1" align='center'> 이미지 </Typography>
+                </Grid>
+                <Grid item xs={7}>
+                  <Typography variant="subtitle1" align='center'> 롤리타, 내 삶의 빛, 내 몸의 불이여. 나의 죄, 나의 영혼이여. 롤-리-타. 혀 끝이 입천장을 따라 세 걸음 걷다 세 걸음째에 앞니를 가볍게 건드린다. 롤. 리. 타. 아침에 양말 한 짝만 신고 서 있을 때 키가 4피트 10인치인 그녀는 로, 그냥 로였다. 슬랙스 차림일 때는 롤라였다. 학교에서는 돌리. 서류상의 이름은 돌로레스. 그러나 내 품에 안길 때는 언제나 롤리타였다  </Typography>
+                </Grid>
+                <br/>
+              </Grid>
+              
+            );
+            })}
+          </Box>
+          <br/><br/><br/>
         </Grid>
-        <Grid item xs={12}>
-          <br />
-          <Paper sx = {{marginBottom : "120px"}}>
-              <br />
-
-
-
-              {isLoading ? <CircularProgress/> : discretes.map((discrete) => {
-              console.log(discrete, resultdata["leadership_mean_by_sector"])
-              const int = resultdata["leadership_mean_by_sector"][discrete["code"]].toFixed(1)
-              const index = appraise_level(int)
-              return(
-              <Grid container className={classes.resultDetail}>
-                {/* <Grid container borderRadius = "10px" marginBottom = "13px"className={classes.resultDetail}border ="3px solid" borderColor = {borderColor[index]} backgroundColor = {backColor2[index]}></Grid> */}
-                <Grid item xs={12}>
-                  <Typography align = "center" marginX = "10px" marginY = "12px"
-                              gutterBottom marginLeft = "12px"
-                              sx = {{fontFamily : "'Source Serif 4'", fontSize : 46, fontWeight : 700}}>
-                                {/* <Typography variant='h4' marginY = "12px" gutterBottom marginLeft = "12px" backgroundColor ={backColor1[index]} ></Typography> */}
-                    {discrete.name} <Rating  precision={0.5} name="read-only" value={Math.floor( int / 10)/2} readOnly />
-                  </Typography>
-
-                </Grid>
-                <Grid item xs = {12} align = "center">
-                  <Paper  align = "left" sx = {{width  : "90%" ,marginBottom : "20px", paddingY : "20px" ,paddingX : "18px"}}>
-                    <Typography sx = {{ fontFamily : "'Source Serif 4'", fontSize : 32, fontWeight : 500}}>
-                    {discrete.level[index].name}
-                    </Typography>
-                <Divider orientation="horizontal"sx = {{borderBottomWidth: 5, borderColor : "#00003360"}} flexItem />
-
-                    {discrete.level[index].detail.split("\n").map((d)=>(
-                      <Typography variant = "body1" marginY = "20px" sx = {{fontFamily : "'Nanum Myeongjo'", fontSize : 17}}>
-                      {d}
-                      </Typography>
-                    ))}
-                  </Paper>
-                </Grid>
-                </Grid>
-              )})}
-          </Paper>
-        </Grid>
-      </Grid>
+        
     </Container>
   )
 }
