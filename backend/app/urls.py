48a5f1181147ecac87d48b1deda4d2b1a68c2be2
dydtkddl <dydtkddhkdwk@naver.com>
2024-04-23 02:59:45 +0900

    123

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -9,8 +9,23 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
        path('', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
 path('home/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
-path('result/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
 path('signin/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/LeadershipSurvey/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/InclusiveLeadershipSurvey/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/JMLeadershipEvaluationSurvey/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/SelfCheck/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/SelfAwareness/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/DifficultiesInEmotion/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('home/UN17Goal/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+
+## 결과 폼
+path('LeadershipSurveyResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('InclusiveLeadershipSurveyResultResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('JMLeadershipEvaluationSurveyResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('SelfCheckResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('SelfAwarenessResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('DifficultInEmotionResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
+path('UN17GoalResult/', TemplateView.as_view(template_name = 'index.html')) ,#추가된 부분
 
    path('signup/', SignUp.as_view()),
     path('login/', SignIn.as_view()),
