8b19a9ee65848d20ecbd03eae6e7125745e8dab5
dydtkddl <dydtkddhkdwk@naver.com>
2024-04-23 03:22:43 +0900

    123

--- frontend/src/pages/Surveys/UN17Goal/BarChartUN17Goal.jsx
+++ frontend/src/pages/Surveys/UN17Goal/BarChartUN17Goal.jsx
@@ -98,7 +98,7 @@ function BarChartUN17Goal(props) {
 
     ];
     console.log(data)
-    const labels = Object.values(data["userdata"]).map((index)=>{
+    const labels = Object.values(data["userdata"]).map((a, index)=>{
       return (`Goal ${index + 1}`)
     });
     const datasets = userData.map(user => ({
