4f1054411fd50758df55b45b791e9c401cb3edc9
dydtkddl <dydtkddhkdwk@naver.com>
2024-05-08 09:32:15 +0900

    123

--- backend/app/views.py
+++ backend/app/views.py
@@ -31,10 +31,8 @@ from config.settings import SECRET_KEY
 class SignUp(APIView):
     def post(self, request):
         data = json.loads(request.body)
-        print(data)
         try:
             if User.objects.filter(userid = data['userid']).exists():
-                print(1)
                 return JsonResponse({"message" : "EXISTS_ID"}, status=201)
             User.objects.create(
             userid 	 = data['userid'],
@@ -50,13 +48,11 @@ class SignUp(APIView):
 class SignIn(APIView):
     def post(self, request):
         data = json.loads(request.body)
-        print(data)
         try:
             if User.objects.filter(userid=data["userid"]).exists():
                 user = User.objects.get(userid=data["userid"])
                 if bcrypt.checkpw(data['password'].encode('UTF-8'), user.password.encode('UTF-8')):
                     token = jwt.encode({'user' : user.userid}, SECRET_KEY, algorithm='HS256')
-                    print(token)
                     ## 로그인 성공시
                     return JsonResponse({"message" : "success" , "token" : token, "name" : user.name}, status=200)
                                                                                                                                     ## 이제 토큰을 프론트에서 local storage에 저장하는 코드 작성하면됌
@@ -93,10 +89,8 @@ class TokenAuthentication(BaseAuthentication):
             raise AuthenticationFailed('No such user')
         return (user, None)
 class RecieveData(APIView):
-    print("RecieveData view")
     authentication_classes = [TokenAuthentication]
     def post(self, request):
-        print("method POST")
         data = json.loads(request.body)
         # 1. 유효한 유저가 요청을 보냈는지 확인
         try:
@@ -107,22 +101,17 @@ class RecieveData(APIView):
         
         # 2. 해당 서베이가 데이터베이스에 존재하는지부터 확인 (POST 요청을 막보내는 경우 차단)
         survey_name = data.get("survey_name")
-        print(survey_name)
         if Survey.objects.filter(survey_name = survey_name).exists():
             if (len(Survey.objects.filter(survey_name = survey_name) )== 1):
                 survey = Survey.objects.get(survey_name = survey_name)
                 questions = survey.question_set.all()
-                print(questions)
                 responses_to_save = []
                 flag = 0
                 for key, value in data["data"].items():
-                    print(key, value)
 
                     q = questions.filter(question_code=key)
                     # question이 있는지 확인
-                    print(len(q))
                     if len(q) == 1:
-                        print(1)
 
                         q= q.first()
                         # 해당 사용자가 해당 질문에 대해서 답한 이력이 있는지 확인 
@@ -134,8 +123,6 @@ class RecieveData(APIView):
                             flag = 1
                     else:
                         # 동일 question이 데이터베이스상에 2개 있으면 에러
-                        print(key, value)
-                        print("문제가 발생하여 데이터를 저장할 수 없습니다.")
                         return JsonResponse({"error": "DataBase Error / Same Question name duplicated. (to backend team : plz modify question name which duplicate one onother)"}, status=400)
 
                 # 모든 데이터가 성공적으로 저장될 수 있는지 확인
@@ -147,12 +134,9 @@ class RecieveData(APIView):
                                 res.objects.get(user = userob, question = q).delete()
                             except:
                                 print("존재 x : 따라서 지울수 없음")
-                            print(q.question_code, "삭제")
                     # 모든 저장리스트 데이터, 데이터베이스 상에 실제 반영
                     for response in responses_to_save:
-                        print(response.question)
                         response.save()
-                    print("모든 데이터가 성공적으로 저장되었습니다.")
                     return JsonResponse({"success": survey_name}, status=200)
                 # request로 넘겨받은 답변 데이터 개수와, 저장할 수 있는 데이터개수가 다르면 취소 (atomic process)
                 else:
@@ -167,7 +151,6 @@ class send_result(APIView):
     def post(self, request):
         data = json.loads(request.body)
         survey_name = data["survey_name"]
-        print(survey_name)
         survey = Survey.objects.get(survey_name = survey_name)
         user = request.user
         userob = User.objects.get(userid = user.userid)
@@ -194,7 +177,6 @@ class send_result(APIView):
                     questiondetaillist.append(questiondetail)
                     usersval = res.objects.filter(question = question, user = userob)
                     if (len(usersval) == 0):
-                        print(1)
                         return JsonResponse({"error" : "일부 데이터 손실"}, status  = 400)
                     resval = usersval.first()
                     resall = res.objects.filter(question=question).aggregate(Avg('value'))["value__avg"]
@@ -205,7 +187,6 @@ class send_result(APIView):
                 data["userdata"] = resdic 
                 data["otherdata"] = resalldic
                 data["questiondetail"] = questiondetaillist
-                print(data)
 
             return JsonResponse(data,status = 200)
         elif (survey_name == "InclusiveLeadershipSurvey"):
@@ -227,17 +208,12 @@ class send_result(APIView):
                 data["other"] = other
                 data["username"] = userob.name
                 data["questiondetail"] = questiondetaillist
-                print(data)
 
             return JsonResponse(data,status = 200)
         elif (survey_name == "JMLeadershipEvaluationSurvey"):
-            print(1)
             if (len(questions)!= 0):
-                print(questions, userob.name)
                 for question in questions:
                     usersval = res.objects.filter(question = question, user = userob)
-                    print(usersval)
-                    print(question.question_code)
                     if (len(usersval) == 0):
                         return JsonResponse({"error" : "일부 데이터 손실"}, status  = 400)
                     resval = usersval.first()
@@ -249,10 +225,8 @@ class send_result(APIView):
                 keys = data.keys()
                 ddddd = {}
                 for key in keys:
-                    print(key)
                     ddddd[key] = {"user" : data[key], "other" : other[key]}
                 ddddd["username"] = userob.name
-                print(ddddd)
             return JsonResponse(ddddd,status = 200)
         else:
             return JsonResponse({"error":"error"}, status =400)
@@ -275,11 +249,8 @@ class send_to_home(APIView):
             for field in all_fields:
                 field_name = field.name
                 field_value = surveydict.get(field_name)
-                print(1)
                 try:
                     field_value = json.loads(field_value.replace('\r', '').replace('\n', '').strip())
-                    print(field_value)
-                    print(type(field_value))
                 except:
                     field_value = field_value
                 sd[field_name] = field_value
@@ -339,12 +310,8 @@ class send_to_survey_form(APIView):
                 for field in question_fields:
                     field_name = field.name
                     field_value = questiondict.get(field_name)
-                    print(1)
                     try:
                         field_value = json.loads(field_value.replace('\r', '').replace('\n', '').strip())
-
-                        print(field_value)
-                        print(type(field_value))
                     except:
                         field_value = field_value
                     sd[field_name] = field_value
@@ -353,8 +320,6 @@ class send_to_survey_form(APIView):
         else:
             return JsonResponse({"error" : "에러"}, status =404)
 class save_user_answer(APIView):
-    print(123333)
-    print(123333)
     authentication_classes = [TokenAuthentication]
     def post(self, request):
         # try:
@@ -371,10 +336,8 @@ class save_user_answer(APIView):
                 qcode = question.question_code
                 CompletedSurvey.objects.filter(user = userob, survey = survey).delete()
                 res.objects.get(user = userob, question = question).delete()
-                print(qcode + "삭제완료")
         for question in questions:
             qcode = question.question_code
-            print(userAnswer)
             responses.append(res(user = userob, question = question , value = userAnswer[qcode] ))
         res.objects.bulk_create(responses)
         CompletedSurvey(user = userob, survey = survey).save()
@@ -385,7 +348,6 @@ class save_user_answer(APIView):
 class send_completed_survey_list(APIView):
     authentication_classes = [TokenAuthentication]
     def post(self, request):
-        print(123333)
         # try:
         user = request.user
         userob = User.objects.get(userid = user.userid)
@@ -402,7 +364,6 @@ class send_completed_survey_list(APIView):
                 "completedAt" : completesurvey.completed_at,
                 "surveyname" : completesurvey.survey.survey_name,
             })
-        print(data)
         return JsonResponse({"data" : data}, status = 200)
         # except:
         #     return JsonResponse({"error" : "에러"}, status =404)
@@ -412,7 +373,6 @@ class send_completed_survey_list(APIView):
 class result_data_render(APIView):
     authentication_classes = [TokenAuthentication]
     def post(self, request):
-        print(123333)
         data = json.loads(request.body)
         user = request.user
         userob = User.objects.get(userid = user.userid)
@@ -426,7 +386,28 @@ class result_data_render(APIView):
             senddata["user"] = group_calc(request_group_info, userob, survey_name)
             senddata["other"] = group_calc(request_group_info, userob, survey_name, target = "other")
             senddata["total"] = sum([ v for v in senddata["user"].values()])/len(senddata["user"].values())
-        print(senddata)
+        return JsonResponse({"senddata" : senddata}, status = 200)
+class result_data_render2(APIView):
+    authentication_classes = [TokenAuthentication]
+    def post(self, request):
+        data = json.loads(request.body)
+        user = request.user
+        userob = User.objects.get(userid = user.userid)
+        request_group_info = data["group_info_"]
+        senddata = {"user" : {}, "other" : {}, "total" : 0 }
+        survey_name = data["surveyname_"]
+        for key,questions_sec in request_group_info.items():
+            count = 0
+            sum = 0
+            for question_title, sub_questions in questions_sec.items():
+                for sub_question in sub_questions :
+                    q = Question.objects.get(question_code = sub_question)
+                    val = res.objects.get(user = userob, question = q)
+                    count +=1
+                    sum += val 
+            mean = sum/count
+        senddata["user"] = group_calc(request_group_info, userob, survey_name)
+        senddata["total"] = sum([ v for v in senddata["user"].values()])/len(senddata["user"].values())
         return JsonResponse({"senddata" : senddata}, status = 200)
       
         # except:
@@ -442,7 +423,6 @@ class send_feedback(APIView):
         user = request.user
         userob = User.objects.get(userid = user.userid)
         feedback = data["feedback"]
-        print(feedback)
         FeedBack(feedback = feedback , user = userob).save()
         return JsonResponse({"message" : "success"}, status = 200)
       
@@ -455,7 +435,6 @@ class send_feedback(APIView):
 def group_calc(request_group_info, userob,survey_name,target = "user"):
     dic = {}
     for group_name, q_codes in request_group_info.items():
-        print(group_name)
         sum_ = 0
         if (survey_name == "PersonalInformationSurvey"):
             question = Question.objects.get(question_code = q_codes[0])
@@ -524,14 +503,11 @@ def result_process_JMLeadershipEvaluationSurvey(resdic):
             Ldic[section_key][q_type]["sum"]+=float(value)
             Ldic[section_key][q_type]["count"]+=1
     leadership_mean = {}
-    print(Ldic.items())
     for key, value in Ldic.items():
-        print(key, value)
         if key not in leadership_mean:
             leadership_mean[key] = {}
         for k, v in Ldic[key].items():
             leadership_mean[key][k]= v["sum"]/v["count"]
-    print(leadership_mean)
     return  leadership_mean
     
 def result_process_InclusiveLeadershipSurvey(resdic):
@@ -548,7 +524,6 @@ def result_process_InclusiveLeadershipSurvey(resdic):
     leadership_mean = {}
     for key, value in Ldic.items():
         leadership_mean[key] = value["sum"]/value["count"]
-    print(leadership_mean)
     return {"mean_by_sector" :  leadership_mean}
     
 
@@ -566,7 +541,6 @@ def result_process_LeadershipSurvey(resdic):
     Ldic = {}
     for key,value in resdic.items():
         q_type = re.sub(r'\d+$', '', key)
-        print(q_type)
         if (q_type in lis):
             if (q_type not in Ldic):
                 Ldic[q_type] = {"sum" : float(value), "count" : 1}
@@ -578,7 +552,6 @@ def result_process_LeadershipSurvey(resdic):
     leadership_mean = {}
     for key, value in Ldic.items():
         leadership_mean[key] = value["sum"]/value["count"]
-    print(leadership_mean)
     return { "leadership_score_self" :leadership_score_self, "leadership_mean_by_sector" :  leadership_mean}
 
 ## 6. 아이디 유효성
@@ -606,13 +579,10 @@ def jmdata(request):
             sep.append(ll) 
             ll = []
     aaaa = []
-    print(sep)
     for s in sep:
         try:
             databasekey = s[0]
             qcount = (len(s)-3)/2
-            print(qcount)
-            print(databasekey)
             kotitle = s[1]
             entitle = s[2]
             dic = {"databasekey" : databasekey, "entitle" : entitle, "kotitle" : kotitle, "questions" : []}
@@ -625,7 +595,6 @@ def jmdata(request):
             aaaa.append(dic)
         except:
             pass
-    print(aaaa)
 
     for d in aaaa:
         databasekey = d["databasekey"]
